6cc3376bf1a827396c3513fb8e1198811bbd5887
zhangxingcheng <1919404416@qq.com>
2023-03-27 21:30:53 +0800

    补充

--- test_29/test_29/test.c
+++ test_29/test_29/test.c
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
 //c语言是结构化的程序设计语言
 // 顺序结构
 // 
@@ -10,14 +11,84 @@
 // if
 // switch
 //
+//int main()
+//{
+//	int age = 60;
+//	if (age < 18)
+//		printf("少年\n");
+//	else if (age >= 18 && age < 26)  //18<=age<26 会先执行18<=age结果为真为1，然后1<26成立，打印青年
+//		printf("青年\n");
+//	else if (age >= 26 && age < 40)
+//		ptinrf("中年\n");
+//	else if (age >= 40 && age < 60)
+//		ptinrf("状年\n");
+//	else if (age >= 60 && age < 100)
+//		ptinrf("老年\n");
+//	else
+//	{
+//		ptintf("老不死\n");
+//	}
+//
+//
+//	//if (age >= 18)
+//	//	printf("成年\n");
+//	//if (age >= 18)
+//	//
+//	//	printf("成年\n");
+//	//else
+//	//	printf("未成年\n");
+//	//	printf("不能谈恋爱\n");//{}可以让else和if控制多条语句，否则默认执行一条语句
+//	
+//	return 0;
+// }
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 2;
+//	if (a == 1)
+//		if (b == 2)
+//			printf("hehe\n");
+//		else
+//			printf("haha\n");
+//	//结果什么都没有打印
+//	//else 和它离的最近的if匹配！！！！！!
+//	//代码风格 - <高质量的C++/C编程指南>
+//	return 0;
+//}
 
 
-
-
-
-
-
-
+//判断一个数是否为奇数
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	if (a%2!=0)
+//	{
+//		printf("%d是奇数", a);
+//	}
+//	else
+//	{
+//		printf("%d不是奇数", a);
+//	}
+//
+//
+//	return 0;
+//}
+//判断1-100之间的奇数
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		if (i % 2 != 0)
+//			printf("%d\n", i);
+//	}
+//
+//
+//
+//	return 0;
+//}
 
 
 
