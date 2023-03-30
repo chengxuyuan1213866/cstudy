8c9cc40416118de4cd56d3fd52fd1fce0aa00224
zhangxingcheng <1919404416@qq.com>
2023-03-30 16:12:17 +0800

    二分查找

--- test_31/test_31/test.c
+++ test_31/test_31/test.c
@@ -213,15 +213,129 @@
 //}
 
 //例题
-int main() {
-	int i = 0;
-	int k = 0;
-	for ( i = 0,k=0;  k=0 ; i++,k++)
+//int main() {
+//	int i = 0;
+//	int k = 0;
+//	for ( i = 0,k=0;  k=0 ; i++,k++)
+//	{
+//		k++;
+//	}
+//	return 0;
+//}
+
+
+//----------do ...  while()循环---------------
+//do
+//    循环语句
+// while(表达式);
+//
+
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		if (i == 5)
+//			//break; //1 2 3 4
+//			continue; // 死循环
+//		printf("%d\n", i);
+//		i++;
+//	} while (i <= 10);
+//	return 0;
+//}
+
+//do语句的特点：循环体至少执行一次
+
+//练习
+//计算n的阶乘
+
+//int main()
+//{
+//	int i = 1;
+//	int s = 1;
+//	int n = 0;
+//	scanf("%d", &n);
+//	for ( i = 1; i <= n ; i++)
+//	{
+//		s *= i;
+//	}
+//	printf("%d", s);
+//
+//	return 0;
+//
+//}
+
+//计算阶乘和
+//int main()
+//{
+//	static int i = 0;
+//	int n = 0;
+//	int sum = 0;
+//	int var = 1;
+	//scanf("%d", &n);
+	//while (i<=n)
+	//{
+	//	for (i = 1; i <= n; i++)
+	//	{
+	//		var *= i;
+	//		sum += var;
+	//	}
+
+	//}
+//	//优化
+//	for (n = 1; n <= 3; n++)
+//	{
+//		var *= n;
+//		sum += var;
+//	}
+//	
+//	printf("%d", sum);
+//	return 0;
+//}
+
+//在一个有序数组中具体查找某个数字
+//二分查找
+
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 7;//要查找的数字
+	//在arr整个数组中查找k的值
+	int sz = sizeof(arr) / sizeof(arr[0]); //数组的元素个数
+	int left = 0;
+	int right = sz - 1;
+
+
+	while (left<=right)
 	{
-		k++;
+		int mid = (left + right) / 2;
+		if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			printf("找到了,下标是%d\n", mid);
+			break;
+
+		}
 	}
+	if (left > right)
+		printf("找不到");
 	return 0;
 }
+
+
+
+
+
+
+
 
 
 
