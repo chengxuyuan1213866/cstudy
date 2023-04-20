e79cd6057e929e5a33153723dcd60a6c983116c6
zhangxingcheng <1919404416@qq.com>
2023-04-20 21:15:51 +0800

    习题

--- test_90/test_90/test.c
+++ test_90/test_90/test.c
@@ -10,47 +10,47 @@
 //strcpy这个库函数，其实返回的是目标空间的起始地址。
 //
 //strcpy模拟实现
-char* my_strcpy(char* dest,const char* src)
-{
-	assert(src != NULL);//断言
-	assert(dest != NULL);
-	char* ret = dest;
-	while (*dest++ = *src++)
-	{
-		;//hello的拷贝
-	}
-	return ret;
-}
-
-//strlen求字符串长度
-//my_strlrn
-
-
-//size_t   是  unsigned int  （无符号整型）
-size_t my_strlen(const char* str)
-{
-	//assert(str != NULL);
-	assert(str);
-	int m = 0;
-	while (*str++ != '\0')
-	{
-		m++;
-
-	}
-	return m;
-}
-
-
-
-int main()
-{
-	char arr1[20] = "xxxxxxxxxxxxxx";
-	char arr2[] = "hello";
-	printf("%zd\n", my_strlen(arr2));
-	//my_strcpy(arr1, arr2);   //1.目标空间的起始地址  2.原空间的起始地址
-	printf("%s\n", my_strcpy(arr1, arr2));  //链式访问
-	return 0;
-}
+//char* my_strcpy(char* dest,const char* src)
+//{
+//	assert(src != NULL);//断言
+//	assert(dest != NULL);
+//	char* ret = dest;
+//	while (*dest++ = *src++)
+//	{
+//		;//hello的拷贝
+//	}
+//	return ret;
+//}
+//
+////strlen求字符串长度
+////my_strlrn
+//
+//
+////size_t   是  unsigned int  （无符号整型）
+//size_t my_strlen(const char* str)
+//{
+//	//assert(str != NULL);
+//	assert(str);
+//	int m = 0;
+//	while (*str++ != '\0')
+//	{
+//		m++;
+//
+//	}
+//	return m;
+//}
+//
+//
+//
+//int main()
+//{
+//	char arr1[20] = "xxxxxxxxxxxxxx";
+//	char arr2[] = "hello";
+//	printf("%zd\n", my_strlen(arr2));
+//	//my_strcpy(arr1, arr2);   //1.目标空间的起始地址  2.原空间的起始地址
+//	printf("%s\n", my_strcpy(arr1, arr2));  //链式访问
+//	return 0;
+//}
 
 
 
@@ -91,11 +91,95 @@ int main()
 //	return 0;
 //}
 
+//-------------------------------------作业  例题-------------------------------------------
 
 
+//int main()
+//{
+//	int a, b, c;
+//	a = 5;
+//	c = ++a;
+//	b = ++c, c++, ++a, a++;
+//	b += a++ + c;
+//	printf("a = %d  b = %d  c = %d", a, b, c);
+//	//9 23 8
+//	return 0;
+//}
+
+//输入一个整数，输出该数32位二进制表示中1的个数，其中负数用补码表示
+
+//int NumberOf1(int n)
+//{
+//    //只对正数有用
+//	int count = 0;
+//	while (n)
+//	{
+//		if (n%2 == 1)
+//		{
+//			count++;
+//		}
+//		n /= 2;
+//	}
+//	return count;
+//}
 
 
+//int NumberOf1(unsigned int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		if (n%2 == 1)
+//		{
+//			count++;
+//		}
+//		n /= 2;
+//	}
+//	return count;
+//}
 
+//int NumberOf1(unsigned int n)
+//{
+//	int count = 0;
+//	int i = 0;
+//	for ( i = 0; i < 32; i++)
+//	{
+//		if(((n >> i)&1)==1);
+//		{
+//			count++;
+//		}
+//	}
+//
+//	return count;
+//}
+//
+//int NumberOf1(unsigned int n)
+//{
+//	int count = 0;
+//	int i = 0;
+//	while (n)
+//	{
+//		n = n & (n - 1);   //!!!!!!!!!!!!!!去掉二进制的最后一个1
+//		count++;
+//	}
+//
+//	return count;
+//}
+//
+//
+//
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = NumberOf1(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+//求两个二进制中不同位的个数
 
 
 
